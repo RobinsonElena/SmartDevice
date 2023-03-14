63c8ded3aea20bb33dd968885cc5a1dc51dcfced
RobinsonElena <sherbakovaen2011@yandex.ru>
2023-03-14 11:33:14 -0400

    Добавляет блок о компании

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {initAccordions} from './modules/accordion/init-accordion';
 
 // ---------------------------------
 
@@ -21,8 +22,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    initAccordions();
   });
 });
+
 
 // ---------------------------------
 
